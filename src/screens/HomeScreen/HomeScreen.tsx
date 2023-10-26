ade2d0be06c98d99ae3620dfcd6cc14a87b38cee
MarcelinoGNeto <marcelino.gneto@gmail.com>
2023-10-26 17:58:37 -0300

    parte 1

---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,30 @@
+import Box from "@src/components/Box/Box";
+import Background from "./patterns/Background/Background";
+import Menu from "./patterns/Menu/Menu";
+import Text from "@src/components/Text/Text";
+import Feed from "./patterns/Feed/Feed";
+import Footer from "./patterns/Footer/Footer";
+
+export default function HomeScreen() {
+  return (
+    <Box 
+    tag="main" 
+    styleSheet={{
+      backgroundColor: "grey",
+      flex: 1,
+    alignItems: "center"
+    }}
+    >
+      <Background />
+      <Menu />
+      <Feed>
+        <Feed.Header />
+        <Text tag="h2" variant="display1">
+          Últimas Atualizações
+        </Text>
+        <Feed.Posts />
+      </Feed>
+      <Footer />
+    </Box>
+  )
+}
---
+++ src/theme/theme.tsx
@@ -0,0 +1,7 @@
+import { typography } from './defaults/typography';
+
+const theme = {
+  typography,
+}
+
+export default theme;
